cbe03c67af0a868cc2a9ccd23b04d402e5a4b21c
Wendy Lau <wlau903@gmail.com>
2017-12-13 03:40:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,72 @@
+let store = { drivers: [], passengers: [], trips: [] };
+
+let driverId = 0;
+class Driver {
+  constructor(name) {
+    this.id = ++driverId,
+    this.name = name
+
+    store.drivers.push(this);
+  }
+
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId === this.id
+    });
+  }
+
+  passengers() {
+    return this.trips().map(trip => {
+      return trip.passenger()
+    });
+  }
+}
+
+let passengerId = 0;
+class Passenger {
+  constructor(name) {
+    this.id = ++passengerId,
+    this.name = name
+
+    store.passengers.push(this);
+  }
+
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.passengerId === this.id
+    })
+  }
+
+  drivers() {
+    return this.trips().map(trip => {
+      return trip.driver()
+    })
+  }
+}
+
+let tripId = 0;
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripId
+    if(driver){
+      this.driverId = driver.id
+    }
+    if(passenger){
+      this.passengerId = passenger.id
+    }
+
+    store.trips.push(this);
+  }
+
+  driver() {
+    return store.drivers.find(function(driver) {
+      return driver.id === this.driverId
+    }.bind(this))
+  }
+
+  passenger() {
+    return store.passengers.find(function(passenger) {
+      return passenger.id === this.passengerId
+    }.bind(this))
+  }
+}
